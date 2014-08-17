509d6518b530a71b15bf7a1d6cca4756e0c3b927
Trismegiste <flo@trismegiste.net>
2014-08-17 10:35:48 +0200

    prototyping for likes on published content

---
+++ src/Trismegiste/SocialBundle/Controller/FamousController.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * Iinano
+ */
+
+namespace Trismegiste\SocialBundle\Controller;
+
+use Trismegiste\SocialBundle\Controller\Template;
+use Trismegiste\Socialist\Famous;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * FamousController is a controller to manage action on entities with Famous contract
+ */
+class FamousController extends Template
+{
+
+    protected function addFanOn(Famous $obj)
+    {
+        $obj->addFan($this->getAuthor());
+    }
+
+    protected function removeFanOn(Famous $obj)
+    {
+        $obj->removeFan($this->getAuthor());
+    }
+
+    public function addFanOnCommentAction(Request $request)
+    {
+        
+    }
+
+    public function addFanOnPublishAction(Request $request)
+    {
+        $pk = $request->get('id');
+        $doc = $this->getRepository()->findByPk($pk);
+        $this->addFanOn($doc);
+        $this->getRepository()->persist($doc);
+
+        return $this->redirectRouteOk('publish_detail', ['pk' => $pk]);
+    }
+
+}
--- src/Trismegiste/SocialBundle/Controller/Template.php
+++ src/Trismegiste/SocialBundle/Controller/Template.php
@@ -18,6 +18,11 @@ abstract class Template extends Controller
         throw new \LogicException('Doctrine is not here');
     }
 
+    /**
+     * Returns the main repository
+     * 
+     * @return \Trismegiste\Yuurei\Persistence\RepositoryInterface
+     */
     protected function getRepository()
     {
         return $this->get('dokudoki.repository');
@@ -31,6 +36,11 @@ abstract class Template extends Controller
     protected function redirectRouteOk($name, $param = [])
     {
         return $this->redirect($this->generateUrl($name, $param));
+    }
+
+    protected function getAuthor()
+    {
+        return $this->getUser()->getAuthor();
     }
 
 }
