9b05f9eda45df5e4c3a1f6a790208e2d466fd666
Trismegiste <flo@trismegiste.net>
2014-08-18 19:21:59 +0200

    crud for simple post

---
+++ src/Trismegiste/SocialBundle/Controller/ContentController.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * Iinano
+ */
+
+namespace Trismegiste\SocialBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * ContentController is a template for the wall/dashboard
+ *
+ * The user NEEDS to be authenticated
+ */
+class ContentController extends Template
+{
+
+    public function indexAction()
+    {
+        return $this->render('TrismegisteSocialBundle:Content:index.html.twig', []);
+    }
+
+    public function render($view, array $parameters = array(), Response $response = null)
+    {
+        $repo = $this->getRepository();
+        $it = $repo->find([]);
+
+        $parameters['listing'] = $it;
+
+        return $this->container->get('templating')->renderResponse($view, $parameters, $response);
+    }
+
+}
