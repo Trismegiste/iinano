f09c8950b1ed2f28e4c3d6a8b6e5f73263be85bb
Trismegiste <flo@trismegiste.net>
2014-08-20 02:13:23 +0200

    edit commentary features

--- src/Trismegiste/SocialBundle/Controller/CommentaryController.php
+++ src/Trismegiste/SocialBundle/Controller/CommentaryController.php
@@ -46,7 +46,29 @@ class CommentaryController extends ContentController
 
     public function editOnPublishingAction($id, $uuid)
     {
-        
+        $pub = $this->getRepository()->findByPk($id);
+        $commentary = $pub->getCommentaryByUuid($uuid);
+
+        $form = $this->createForm(new CommentaryForm(), $commentary);
+
+        $form->handleRequest($this->getRequest());
+        if ($form->isValid()) {
+            try {
+                $this->getRepository()->persist($pub);
+                $this->pushFlash('notice', 'Message saved');
+                return $this->redirectRouteOk('content_index', [], 'anchor-' . $id);
+            } catch (\MongoException $e) {
+                $this->pushFlash('warning', 'Cannot save message');
+            }
+        }
+
+        $param = [
+            'publishing' => $pub,
+            'skipped_pub' => $id,
+            'form' => $form->createView()
+        ];
+
+        return $this->render('TrismegisteSocialBundle:Content:commentary_form.html.twig', $param);
     }
 
 }
