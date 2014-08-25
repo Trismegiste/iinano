6d732f39cedbf4317855a5aebc87ab971bd6b4be
Trismegiste <flo@trismegiste.net>
2014-08-25 04:03:05 +0200

    a simple test for createuser command

---
+++ src/Trismegiste/SocialBundle/Tests/Cli/CreateUserTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * Iinano
+ */
+
+namespace Trismegiste\SocialBundle\Tests\Cli;
+
+use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Trismegiste\SocialBundle\Cli\CreateUser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * CreateUserTest tests CreateUser
+ */
+class CreateUserTest extends WebTestCase
+{
+
+    public function testExecute()
+    {
+        $kernel = $this->createKernel();
+        $kernel->boot();
+
+        $application = new Application($kernel);
+        $application->add(new CreateUser());
+        $command = $application->find('social:user:create');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'command' => $command->getName(),
+            'nickname' => 'scotty'
+        ]);
+        $this->assertRegExp('/.../', $commandTester->getDisplay());
+    }
+
+}
