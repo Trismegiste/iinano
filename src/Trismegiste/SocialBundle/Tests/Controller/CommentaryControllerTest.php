6f5238e352b621cb71b13e546c75799c353f99cd
Trismegiste <flo@trismegiste.net>
2014-08-27 16:02:57 +0200

    test hacking url on commentary

--- src/Trismegiste/SocialBundle/Tests/Controller/CommentaryControllerTest.php
+++ src/Trismegiste/SocialBundle/Tests/Controller/CommentaryControllerTest.php
@@ -36,6 +36,7 @@ class CommentaryControllerTest extends WebTestCasePlus
         $this->getService('dokudoki.repository')->persist($post);
         $this->assertCount(1, $this->collection->find());
         $this->addUserFixture('kirk');
+        $this->addUserFixture('spock');
 
         return (string) $post->getId();
     }
@@ -97,6 +98,64 @@ class CommentaryControllerTest extends WebTestCasePlus
         $this->assertInstanceOf('Trismegiste\Socialist\SimplePost', $restore);
         $comment = $restore->getCommentary();
         $this->assertCount(0, $comment);
+
+        return $pk;
+    }
+
+    /**
+     * @depends testDeleteCommentary
+     */
+    public function testOtherAddCommentary($pk)
+    {
+        $this->login('spock');
+        $crawler = $this->getPage('content_index');
+        $link = $crawler->selectLink('Reply')->link();
+        $crawler = $this->client->click($link);
+        $form = $crawler->selectButton('Save')->form();
+        $this->client->submit($form, ['commentary' => ['message' => __METHOD__]]);
+
+        $restore = $this->getService('dokudoki.repository')->findByPk($pk);
+        $this->assertInstanceOf('Trismegiste\Socialist\SimplePost', $restore);
+        $comment = $restore->getCommentary();
+        $this->assertCount(1, $comment);
+        $comment = $comment[0];
+        $this->assertEquals(__METHOD__, $comment->getMessage());
+        $this->assertEquals('spock', $comment->getAuthor()->getNickname());
+    }
+
+    public function testNoEditOnCommentaryFromOther()
+    {
+        $crawler = $this->getPage('content_index');
+        $this->assertCount(0, $crawler->filter('div.commentary')->selectLink('Edit'));
+    }
+
+    public function testNoDeleteOnCommentaryFromOther()
+    {
+        $crawler = $this->getPage('content_index');
+        $this->assertCount(0, $crawler->filter('div.commentary')->selectLink('Delete'));
+    }
+
+    public function testHackEditCommentaryFromOther()
+    {
+        $crawler = $this->getPage('content_index');
+        $anchor = $crawler->filter("div.commentary a[id^=anchor]")
+                        ->eq(0)->attr('id');
+        preg_match('#^anchor-([\da-f]{24})-([\da-f]{40})$#', $anchor, $match);
+        $pk = ['id' => $match[1], 'uuid' => $match[2]];
+        // try to get the form edit
+        $crawler = $this->getPage('pub_commentary_edit', $pk);
+        $this->assertEquals(403, $this->client->getResponse()->getStatusCode());
+
+        return $pk;
+    }
+
+    /**
+     * @depends testHackEditCommentaryFromOther
+     */
+    public function testHackDeleteCommentaryFromOther($pk)
+    {
+        $crawler = $this->getPage('pub_commentary_delete', $pk);
+        $this->assertEquals(403, $this->client->getResponse()->getStatusCode());
     }
 
 }
