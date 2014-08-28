5c942815f04576a6d0c6cbb397071a0c77ff0303
Trismegiste <flo@trismegiste.net>
2014-08-28 21:43:38 +0200

    testing xsend with avatar

--- src/Trismegiste/SocialBundle/Controller/NetizenController.php
+++ src/Trismegiste/SocialBundle/Controller/NetizenController.php
@@ -7,6 +7,8 @@
 namespace Trismegiste\SocialBundle\Controller;
 
 use Trismegiste\SocialBundle\Controller\Template;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * NetizenController is a controller for the user : profile, stats...
@@ -17,6 +19,18 @@ class NetizenController extends Template
     public function showProfileAction()
     {
         return $this->render('TrismegisteSocialBundle:Netizen:profile_show.html.twig');
+    }
+
+    public function sendAvatarAction()
+    {
+        $basePath = $this->container->getParameter('kernel.root_dir') . '/static/';
+        $file = $basePath . 'hinaitigo.png';
+
+        $response = new Response();
+        $response->headers->set('X-Sendfile', $file);
+        $response->headers->set('Content-Type', 'image/png');
+
+        return $response;
     }
 
 }
