3da7a354e1d41d23810917f95d78f0b50b00c9d6
Trismegiste <flo@trismegiste.net>
2014-08-28 13:01:43 +0200

    theming form errors

---
+++ src/Trismegiste/SocialBundle/Form/SimplePostType.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * Iinano
+ */
+
+namespace Trismegiste\SocialBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+
+/**
+ * SimplePostType is a form for SimplePost
+ */
+class SimplePostType extends AbstractType
+{
+
+    public function buildForm(\Symfony\Component\Form\FormBuilderInterface $builder, array $options)
+    {
+        $builder->add("title", 'text', ['constraints' => [
+                        new NotBlank(),
+                        new Length(['min' => 3, 'max' => 40])
+            ]])
+                ->add('body', 'textarea', ['constraints' => [
+                        new NotBlank(),
+                        new Length(['min' => 10, 'max' => 480])
+            ]])
+                ->add('save', 'submit');
+    }
+
+    public function getName()
+    {
+        return 'simple_post';
+    }
+
+}
