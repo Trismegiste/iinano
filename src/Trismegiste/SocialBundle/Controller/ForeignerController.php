0111f5c1985f04bfa8e723498c3898576e7380b2
Trismegiste <flo@trismegiste.net>
2014-08-28 13:25:56 +0200

    theme for inline form

--- src/Trismegiste/SocialBundle/Controller/ForeignerController.php
+++ src/Trismegiste/SocialBundle/Controller/ForeignerController.php
@@ -8,6 +8,7 @@ namespace Trismegiste\SocialBundle\Controller;
 
 use Trismegiste\SocialBundle\Controller\Template;
 use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * ForeignerController is a controller for unathentificated user
@@ -35,9 +36,15 @@ class ForeignerController extends Template
         return $this->render('TrismegisteSocialBundle:Foreigner:login.html.twig', ['error' => $error]);
     }
 
-    public function registerAction()
+    public function registerAction(Request $request)
     {
         $form = $this->createForm(new \Trismegiste\SocialBundle\Form\RegisterType());
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            
+        }
 
         return $this->render('TrismegisteSocialBundle:Foreigner:register.html.twig', ['register' => $form->createView()]);
     }
