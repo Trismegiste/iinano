f927a122cf858d7d4b910bbbc65240041c97829d
Trismegiste <flo@trismegiste.net>
2014-09-01 12:36:33 +0200

    refactor pagination

--- src/Trismegiste/SocialBundle/Controller/ContentController.php
+++ src/Trismegiste/SocialBundle/Controller/ContentController.php
@@ -9,6 +9,7 @@ namespace Trismegiste\SocialBundle\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Trismegiste\Socialist\Content;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Trismegiste\SocialBundle\Utils\SkippableIterator;
 
 /**
  * ContentController is a template for the wall/dashboard
@@ -18,6 +19,12 @@ use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 class ContentController extends Template
 {
 
+    protected function getPagination()
+    {
+        // @todo add a config somewhere
+        return 20;
+    }
+
     public function indexAction()
     {
         return $this->render('TrismegisteSocialBundle:Content:index.html.twig', []);
@@ -25,14 +32,13 @@ class ContentController extends Template
 
     public function ajaxMoreAction($offset)
     {
+        // @todo problem with dynamic tooltip
         if (!$this->getRequest()->isXmlHttpRequest()) {
             throw new AccessDeniedException('U haxxor');
         }
 
         $repo = $this->getRepository();
-        $it = $repo->findLastEntries($offset);
-
-        $parameters['listing'] = $it;
+        $parameters['listing'] = $repo->findLastEntries($offset, $this->getPagination());
 
         return parent::render('TrismegisteSocialBundle:Content:index_more.html.twig', $parameters);
     }
@@ -40,12 +46,13 @@ class ContentController extends Template
     public function render($view, array $parameters = array(), Response $response = null)
     {
         $repo = $this->getRepository();
-        $it = $repo->findLastEntries();
+        $it = $repo->findLastEntries(0, $this->getPagination());
 
         if (array_key_exists('skipped_pub', $parameters)) {
-            $it = new \Trismegiste\SocialBundle\Utils\SkippableIterator($it, [$parameters['skipped_pub']]);
+            $it = new SkippableIterator($it, [$parameters['skipped_pub']]);
         }
         $parameters['listing'] = $it;
+        $parameters['pagination'] = $this->getPagination();
 
         return parent::render($view, $parameters, $response);
     }
