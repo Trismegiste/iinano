5ec204bb0f13796057c42367a1360c9afc0ce181
Trismegiste <flo@trismegiste.net>
2014-09-07 04:04:11 +0200

    test wall entries and filter on author

--- src/Trismegiste/SocialBundle/Tests/Unit/Repository/PublishingRepositoryTest.php
+++ src/Trismegiste/SocialBundle/Tests/Unit/Repository/PublishingRepositoryTest.php
@@ -9,6 +9,7 @@ namespace Trismegiste\SocialBundle\Tests\Unit\Repository;
 use Trismegiste\SocialBundle\Repository\PublishingRepository;
 use Trismegiste\Yuurei\Persistence\CollectionIterator;
 use Trismegiste\Socialist\Author;
+use Trismegiste\SocialBundle\Security\Netizen;
 
 /**
  * PublishingRepositoryTest tests PublishingRepository
@@ -73,6 +74,73 @@ class PublishingRepositoryTest extends \PHPUnit_Framework_TestCase
                 ->with($this->equalTo($this->document));
 
         $this->sut->persist($this->document);
+    }
+
+    private function createNetizen($nick)
+    {
+        $mock = $this->getMock("Trismegiste\Socialist\AuthorInterface");
+        $mock->expects($this->any())
+                ->method('getNickname')
+                ->will($this->returnValue($nick));
+
+        $user = new Netizen($mock);
+        $user->setId(new \MongoId());
+
+        return $user;
+    }
+
+    public function createGraph()
+    {
+        $user = [];
+        foreach (['kirk', 'scotty', 'mccoy', 'spock', 'sulu'] as $nick) {
+            $user[] = $this->createNetizen($nick);
+        }
+
+        $user[1]->follow($user[0]);
+        $user[2]->follow($user[0]);
+        $user[4]->follow($user[0]);
+        $user[0]->follow($user[2]);
+        $user[0]->follow($user[3]);
+
+        return [
+            [$user[0], 'self', ['kirk']],
+            [$user[0], 'following', ['mccoy', 'spock']],
+            [$user[0], 'follower', ['scotty', 'mccoy', 'sulu']],
+            [$user[0], 'friend', ['mccoy']]
+        ];
+    }
+
+    /**
+     * @dataProvider createGraph
+     */
+    public function testFindWallEntriesWithAuthor($user, $wallFilter, $authorName)
+    {
+        $cursor = new CollectionIterator($this->getMock('MongoCursor', [], [], '', false), $this->repository);
+        $this->repository->expects($this->once())
+                ->method('find')
+                ->with(['-class' => ['$in' => ['message']], 'owner.nickname' => ['$in' => $authorName]])
+                ->will($this->returnValue($cursor));
+
+        $this->sut->findWallEntries($user, $wallFilter);
+    }
+
+    public function testFindWallAll()
+    {
+        $cursor = new CollectionIterator($this->getMock('MongoCursor', [], [], '', false), $this->repository);
+        $this->repository->expects($this->once())
+                ->method('find')
+                ->with(['-class' => ['$in' => ['message']]])
+                ->will($this->returnValue($cursor));
+
+        $this->sut->findWallEntries($this->createNetizen('kirk'), 'all');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testInvalidFilter()
+    {
+        $this->sut->findWallEntries($this->createNetizen('kirk'), 'sdsdqs');
     }
 
 }
