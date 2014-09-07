2715aa840e98ef40c8d616dc6efd05c9d5ea447e
Trismegiste <flo@trismegiste.net>
2014-09-07 12:29:02 +0200

    testing bundle

---
+++ src/Trismegiste/SocialBundle/Tests/Unit/TrismegisteSocialBundleTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * Iinano
+ */
+
+namespace Trismegiste\SocialBundle\Tests\Unit;
+
+use Trismegiste\SocialBundle\TrismegisteSocialBundle;
+
+/**
+ * TrismegisteSocialBundleTest tests TrismegisteSocialBundle
+ */
+class TrismegisteSocialBundleTest extends \PHPUnit_Framework_TestCase
+{
+
+    /** @var TrismegisteSocialBundle */
+    protected $sut;
+
+    protected function setUp()
+    {
+        $this->sut = new TrismegisteSocialBundle();
+    }
+
+    public function test()
+    {
+        $container = $this->getMock('Symfony\Component\DependencyInjection\ContainerInterface');
+        $container->expects($this->any())
+                ->method('getParameter')
+                ->will($this->returnValue(true));
+        $this->sut->boot();
+        $this->sut->setContainer($container);
+        $this->sut->getContainerExtension();
+        $this->sut->registerCommands($this->getMock('Symfony\Component\Console\Application'));
+    }
+
+}
