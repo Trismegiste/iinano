c30d5b765d36a7757440b501767977a92137fc2c
Trismegiste <flo@trismegiste.net>
2014-09-07 04:26:02 +0200

    starting test for netizen repo

---
+++ src/Trismegiste/SocialBundle/Tests/Unit/Repository/NetizenRepositoryTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * Iinano
+ */
+
+namespace Trismegiste\SocialBundle\Tests\Unit\Repository;
+
+use Trismegiste\SocialBundle\Repository\NetizenRepository;
+use Trismegiste\SocialBundle\Security\Netizen;
+
+/**
+ * NetizenRepositoryTest tests NetizenRepository
+ */
+class NetizenRepositoryTest extends \PHPUnit_Framework_TestCase
+{
+
+    /** @var NetizenRepository */
+    protected $sut;
+    protected $repository;
+    protected $encoder;
+    protected $storage;
+
+    protected function setUp()
+    {
+        $this->encoder = $this->getMock('Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface');
+        $this->repository = $this->getMock('Trismegiste\Yuurei\Persistence\RepositoryInterface');
+        $this->storage = $this->getMock('Trismegiste\SocialBundle\Repository\AvatarRepository', [], [], '', false);
+
+        $this->sut = new NetizenRepository($this->repository, $this->encoder, ['netizen'], $this->storage);
+    }
+
+    public function test()
+    {
+        $this->sut;
+    }
+
+}
