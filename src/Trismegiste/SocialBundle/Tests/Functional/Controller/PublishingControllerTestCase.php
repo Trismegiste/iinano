87939751f5d428fa1576b81c934ba62b1b07f3fb
Trismegiste <flo@trismegiste.net>
2014-09-23 16:18:16 +0200

    generalizing test for publishing controller

---
+++ src/Trismegiste/SocialBundle/Tests/Functional/Controller/PublishingControllerTestCase.php
@@ -0,0 +1,150 @@
+<?php
+
+/*
+ * Iinano
+ */
+
+namespace Trismegiste\SocialBundle\Tests\Functional\Controller;
+
+use Trismegiste\Socialist\Publishing;
+
+/**
+ * PublishingControllerTestCase is test case for all entities managed by PublishingController
+ */
+abstract class PublishingControllerTestCase extends WebTestCasePlus
+{
+
+    protected $collection;
+    protected $contentRepo;
+    static protected $random;
+
+    static public function setUpBeforeClass()
+    {
+        // each test case has a different random, avoid false positive
+        static::$random = rand();
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->client->followRedirects();
+        $this->logIn('kirk');
+        $this->collection = $this->getService('dokudoki.collection');
+        $this->contentRepo = $this->getService('social.content.repository');
+    }
+
+    abstract protected function getCreateLinkText();
+
+    abstract protected function getFormNewContent();
+
+    abstract protected function assertNewContent(Publishing $doc);
+
+    abstract protected function getFormEditContent();
+
+    abstract protected function assertEditContent(Publishing $doc);
+
+    /**
+     * @test
+     */
+    public function initialize()
+    {
+        $this->collection->drop();
+        $this->assertCount(0, $this->collection->find());
+        $this->addUserFixture('kirk');
+        $this->addUserFixture('spock');
+    }
+
+    public function testCreateFirstPost()
+    {
+        $crawler = $this->getPage('content_index');
+        $link = $crawler->selectLink($this->getCreateLinkText())->link();
+        $crawler = $this->client->click($link);
+        $form = $crawler->selectButton('Save')->form();
+        $this->client->submit($form, $this->getFormNewContent());
+
+        $it = $this->contentRepo->findLastEntries();
+        $this->assertCount(1, $it);
+        $it->rewind();
+        $doc = $it->current();
+
+        $this->assertNewContent($doc);
+        $this->assertEquals('kirk', $doc->getAuthor()->getNickname());
+
+        return (string) $doc->getId();
+    }
+
+    /**
+     * @depends testCreateFirstPost
+     */
+    public function testEdit($pk)
+    {
+        $crawler = $this->getPage('content_index');
+        $link = $crawler->filter('div.publishing')->selectLink('Edit')->link();
+        $crawler = $this->client->click($link);
+        $form = $crawler->selectButton('Save')->form();
+        $this->client->submit($form, $this->getFormEditContent());
+
+        $this->assertCount(1, $this->contentRepo->findLastEntries());
+        $doc = $this->contentRepo->findByPk($pk);
+
+        $this->assertEditContent($doc);
+        $this->assertEquals('kirk', $doc->getAuthor()->getNickname());
+    }
+
+    public function testCreateSecondPost()
+    {
+        $crawler = $this->getPage('content_index');
+        $link = $crawler->selectLink($this->getCreateLinkText())->link();
+        $crawler = $this->client->click($link);
+        $form = $crawler->selectButton('Save')->form();
+        $this->client->submit($form, $this->getFormNewContent());
+
+        $this->assertCount(2, $this->contentRepo->findLastEntries());
+    }
+
+    public function testDeleteFirst()
+    {
+        $crawler = $this->getPage('content_index');
+        $link = $crawler->selectLink('Delete')->link();
+        $crawler = $this->client->click($link);
+
+        $this->assertCount(1, $this->contentRepo->findLastEntries());
+    }
+
+    public function testNoEditFromOther()
+    {
+        //override user
+        $this->logIn('spock');
+        $crawler = $this->getPage('content_index');
+        $this->assertCount(0, $crawler->filter('div.publishing')->selectLink('Edit'));
+    }
+
+    public function testHackEditFromOther()
+    {
+        //override user
+        $this->logIn('spock');
+        $crawler = $this->getPage('wall_index', ['wallNick' => 'spock', 'wallFilter' => 'all']);
+        $anchor = $crawler->filter("div.publishing a[id^=anchor]")
+                        ->eq(0)->attr('id');
+        preg_match('#^anchor-([\da-f]{24})$#', $anchor, $match);
+        $pk = $match[1];
+        // try to get the form edit
+        $crawler = $this->getPage('publishing_edit', ['id' => $pk]);
+        $this->assertEquals(403, $this->client->getResponse()->getStatusCode());
+
+        return $pk;
+    }
+
+    /**
+     * @depends testHackEditFromOther
+     */
+    public function testHackDeleteFromOther($pk)
+    {
+        //override user
+        $this->logIn('spock');
+        // try to delete
+        $crawler = $this->getPage('publishing_delete', ['id' => $pk]);
+        $this->assertEquals(403, $this->client->getResponse()->getStatusCode());
+    }
+
+}
--- src/Trismegiste/SocialBundle/Tests/Functional/Controller/SimplePostControllerTest.php
+++ src/Trismegiste/SocialBundle/Tests/Functional/Controller/SimplePostControllerTest.php
@@ -6,128 +6,47 @@
 
 namespace Trismegiste\SocialBundle\Tests\Functional\Controller;
 
+use Trismegiste\Socialist\Publishing;
+
 /**
  * SimplePostControllerTest tests SimplePostController
  */
-class SimplePostControllerTest extends WebTestCasePlus
+class SimplePostControllerTest extends PublishingControllerTestCase
 {
 
-    protected $collection;
-    protected $contentRepo;
-
-    protected function setUp()
+    protected function getCreateLinkText()
     {
-        parent::setUp();
-        $this->client->followRedirects();
-        $this->logIn('kirk');
-        $this->collection = $this->getService('dokudoki.collection');
-        $this->contentRepo = $this->getService('social.content.repository');
+        return 'Simple Post';
     }
 
-    /**
-     * @test
-     */
-    public function initialize()
+    protected function getFormNewContent()
     {
-        $this->collection->drop();
-        $this->assertCount(0, $this->collection->find());
-        $this->addUserFixture('kirk');
-        $this->addUserFixture('spock');
+        return ['social_simplepost' => [
+                'title' => 'A title ' . static::$random,
+                'body' => 'A body with ' . static::$random
+        ]];
     }
 
-    public function testCreateFirstPost()
+    protected function assertNewContent(Publishing $doc)
     {
-        $crawler = $this->getPage('content_index');
-        $link = $crawler->selectLink('Simple Post')->link();
-        $crawler = $this->client->click($link);
-        $form = $crawler->selectButton('Save')->form();
-        $this->client->submit($form, ['social_simplepost' => ['title' => __CLASS__, 'body' => __METHOD__]]);
-
-        $it = $this->contentRepo->findLastEntries();
-        $this->assertCount(1, $it);
-        $it->rewind();
-        $doc = $it->current();
-
         $this->assertInstanceOf('Trismegiste\Socialist\SimplePost', $doc);
-        $this->assertEquals(__CLASS__, $doc->getTitle());
-        $this->assertEquals(__METHOD__, $doc->getBody());
-        $this->assertEquals('kirk', $doc->getAuthor()->getNickname());
-
-        return (string) $doc->getId();
+        $this->assertEquals('A title ' . static::$random, $doc->getTitle());
+        $this->assertEquals('A body with ' . static::$random, $doc->getBody());
     }
 
-    /**
-     * @depends testCreateFirstPost
-     */
-    public function testEdit($pk)
+    protected function getFormEditContent()
     {
-        $crawler = $this->getPage('content_index');
-        $link = $crawler->filter('div.publishing')->selectLink('Edit')->link();
-        $crawler = $this->client->click($link);
-        $form = $crawler->selectButton('Save')->form();
-        $this->client->submit($form, ['social_simplepost' => ['title' => __CLASS__, 'body' => __METHOD__]]);
-
-        $this->assertCount(1, $this->contentRepo->findLastEntries());
-        $doc = $this->contentRepo->findByPk($pk);
-
-        $this->assertEquals(__METHOD__, $doc->getBody());
-        $this->assertEquals('kirk', $doc->getAuthor()->getNickname());
+        return ['social_simplepost' => [
+                'title' => 'A edited title ' . static::$random,
+                'body' => 'A edited body with ' . static::$random
+        ]];
     }
 
-    public function testCreateSecondPost()
+    protected function assertEditContent(Publishing $doc)
     {
-        $crawler = $this->getPage('content_index');
-        $link = $crawler->selectLink('Simple Post')->link();
-        $crawler = $this->client->click($link);
-        $form = $crawler->selectButton('Save')->form();
-        $this->client->submit($form, ['social_simplepost' => ['title' => __CLASS__, 'body' => __METHOD__]]);
-
-        $this->assertCount(2, $this->contentRepo->findLastEntries());
-    }
-
-    public function testDeleteFirst()
-    {
-        $crawler = $this->getPage('content_index');
-        $link = $crawler->selectLink('Delete')->link();
-        $crawler = $this->client->click($link);
-
-        $this->assertCount(1, $this->contentRepo->findLastEntries());
-    }
-
-    public function testNoEditFromOther()
-    {
-        //override user
-        $this->logIn('spock');
-        $crawler = $this->getPage('content_index');
-        $this->assertCount(0, $crawler->filter('div.publishing')->selectLink('Edit'));
-    }
-
-    public function testHackEditFromOther()
-    {
-        //override user
-        $this->logIn('spock');
-        $crawler = $this->getPage('wall_index', ['wallNick' => 'spock', 'wallFilter' => 'all']);
-        $anchor = $crawler->filter("div.publishing a[id^=anchor]")
-                        ->eq(0)->attr('id');
-        preg_match('#^anchor-([\da-f]{24})$#', $anchor, $match);
-        $pk = $match[1];
-        // try to get the form edit
-        $crawler = $this->getPage('publishing_edit', ['id' => $pk]);
-        $this->assertEquals(403, $this->client->getResponse()->getStatusCode());
-
-        return $pk;
-    }
-
-    /**
-     * @depends testHackEditFromOther
-     */
-    public function testHackDeleteFromOther($pk)
-    {
-        //override user
-        $this->logIn('spock');
-        // try to delete
-        $crawler = $this->getPage('publishing_delete', ['id' => $pk]);
-        $this->assertEquals(403, $this->client->getResponse()->getStatusCode());
+        $this->assertInstanceOf('Trismegiste\Socialist\SimplePost', $doc);
+        $this->assertEquals('A edited title ' . static::$random, $doc->getTitle());
+        $this->assertEquals('A edited body with ' . static::$random, $doc->getBody());
     }
 
 }
