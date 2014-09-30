9c865df1e5d25148ff096a6ddff98f74b9673f46
Trismegiste <flo@trismegiste.net>
2014-09-30 08:46:42 +0200

    typo on phpdoc

--- src/Trismegiste/SocialBundle/Repository/NetizenRepository.php
+++ src/Trismegiste/SocialBundle/Repository/NetizenRepository.php
@@ -32,11 +32,14 @@ class NetizenRepository implements NetizenRepositoryInterface
      *
      * @param RepositoryInterface $repo the repository for MongoCollection
      * @param EncoderFactoryInterface $encoderFactory the Security component factory which manages encoders for password
-     * @param type $alias the class key alias for the Netizen objects stored with Dokudoki
+     * @param string $alias the class key alias for the Netizen objects stored with Dokudoki
      * @param \Trismegiste\SocialBundle\Repository\AvatarRepository $storage a repository for storing avatar pictures
      */
     public function __construct(RepositoryInterface $repo, EncoderFactoryInterface $encoderFactory, $alias, AvatarRepository $storage)
     {
+        if (!is_string($alias)) {
+            throw new \InvalidArgumentException('Alias for Netizen is not a string');
+        }
         $this->repository = $repo;
         $this->classAlias = $alias;
         $this->encoderFactory = $encoderFactory;
