e43ffa1dc198ab926430154f45bc33cdbdb79c4d
Trismegiste <flo@trismegiste.net>
2014-10-03 03:44:25 +0200

    adding small talk entity to the app

---
+++ src/Trismegiste/SocialBundle/Form/SmallTalkType.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * Iinano
+ */
+
+namespace Trismegiste\SocialBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * SmallTalkType is a form for SmallTalk
+ */
+class SmallTalkType extends AbstractType
+{
+
+    public function buildForm(\Symfony\Component\Form\FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('message', 'textarea', ['constraints' => [
+                        new NotBlank(),
+                        new Length(['min' => 10, 'max' => 280])
+            ]])
+                ->add('save', 'submit');
+    }
+
+    public function getName()
+    {
+        return 'social_small';
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(['data_class' => 'Trismegiste\Socialist\SmallTalk']);
+    }
+
+}
---
+++ src/Trismegiste/SocialBundle/Tests/Functional/Controller/SmallTalkControllerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * iinano
+ */
+
+namespace Trismegiste\SocialBundle\Tests\Functional\Controller;
+
+use Trismegiste\Socialist\Publishing;
+
+/**
+ * SmallTalkControllerTest tests tests PublishingController with SmallTalk
+ */
+class SmallTalkControllerTest extends PublishingControllerTestCase
+{
+
+    protected function assertEditContent(Publishing $doc)
+    {
+        $this->assertInstanceOf('Trismegiste\Socialist\SmallTalk', $doc);
+        $this->assertEquals('Kon nichi wa ' . static::$random, $doc->getMessage());
+    }
+
+    protected function assertNewContent(Publishing $doc)
+    {
+        $this->assertInstanceOf('Trismegiste\Socialist\SmallTalk', $doc);
+        $this->assertEquals('Hello from ' . static::$random, $doc->getMessage());
+    }
+
+    protected function getCreateLinkText()
+    {
+        return 'Message';
+    }
+
+    protected function getFormEditContent()
+    {
+        return ['social_small' => [
+                'message' => 'Kon nichi wa ' . static::$random
+        ]];
+    }
+
+    protected function getFormNewContent()
+    {
+        return ['social_small' => [
+                'message' => 'Hello from ' . static::$random
+        ]];
+    }
+
+}
