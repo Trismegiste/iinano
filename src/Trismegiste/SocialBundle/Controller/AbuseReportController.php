6878714822df8b4d7f02a887caf5489f1a153ac9
Trismegiste <flo@trismegiste.net>
2014-10-06 01:02:46 +0200

    just an general idea of the abuse dashbord - not optimized at all

--- src/Trismegiste/SocialBundle/Controller/AbuseReportController.php
+++ src/Trismegiste/SocialBundle/Controller/AbuseReportController.php
@@ -32,8 +32,15 @@ class AbuseReportController extends ContentController
 
     public function listingAction()
     {
-        $listing = $this->getRepository()->findAbusiveReport();
+        $reportRepo = $this->get('social.abusereport.repository');
+        $reportRepo->compileReport();
+        $iterator = $reportRepo->findMostReported(0,30);
 
+        $listing = [];
+        foreach ($iterator as $report) {
+            $report['content'] = $this->getRepository()->findByPk($report['_id']['id']);
+            $listing[] = $report;
+        }
         return $this->render('TrismegisteSocialBundle:AbuseReport:dashboard.html.twig', [
                     'listing' => $listing
         ]);
