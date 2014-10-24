7f22d08a3a1f80e9b0f91b1c39fb3d68c1444df9
Trismegiste <flo@trismegiste.net>
2014-10-24 03:13:37 +0200

    testing with the wall

--- src/Trismegiste/SocialBundle/Controller/PureController.php
+++ src/Trismegiste/SocialBundle/Controller/PureController.php
@@ -9,12 +9,14 @@ namespace Trismegiste\SocialBundle\Controller;
 /**
  * PureController is a ...
  */
-class PureController extends \Symfony\Bundle\FrameworkBundle\Controller\Controller
+class PureController extends ContentController
 {
 
     public function indexAction()
     {
-        return $this->render('TrismegisteSocialBundle::base.html.twig');
+        return $this->render('TrismegisteSocialBundle::base.html.twig', [
+                    'wallUser' => $this->getUser()
+        ]);
     }
 
 }
