89f91c3de73c0998217224d0cb7b3d517bdc5428
Trismegiste <flo@trismegiste.net>
2014-11-18 06:14:37 +0100

    test cached response for picture

--- src/Trismegiste/SocialBundle/Tests/Functional/Controller/PictureControllerTest.php
+++ src/Trismegiste/SocialBundle/Tests/Functional/Controller/PictureControllerTest.php
@@ -45,13 +45,37 @@ class PictureControllerTest extends WebTestCasePlus
         $path = tempnam(sys_get_temp_dir(), 'pic');
         \imagejpeg($img, $path);
         $fh = new UploadedFile($path, 'functest.jpg', null, null, null, true);
-
         $doc = new \Trismegiste\Socialist\Picture($this->createAuthor('spock'));
-
         $this->getService('social.picture.storage')->insertUpload($doc, $fh);
 
         $this->logIn('kirk');
-        $this->getPage('picture_get', ['size' => 'full', 'storageKey' => $doc->getStorageKey()]);
+        $route = $this->generateUrl('picture_get', ['size' => 'full', 'storageKey' => $doc->getStorageKey()]);
+
+        $this->client->request('GET', $route);
+        $response = $this->client->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('image/jpeg', $response->headers->get('Content-Type'));
+        $this->assertTrue($response->headers->has('X-Sendfile'));
+        $lastModif = $response->getLastModified();
+        $this->assertNotNull($lastModif);
+
+        return [$doc->getStorageKey(), $lastModif, $response->headers->get('ETag')];
+    }
+
+    /**
+     * @depends testFirstRequest
+     */
+    public function testCachedResponse($param)
+    {
+        list($key, $last, $etag) = $param;
+        $this->logIn('kirk');
+        $route = $this->generateUrl('picture_get', ['size' => 'full', 'storageKey' => $key]);
+
+        $this->client->request('GET', $route, [], [], [
+            'HTTP_If-Modified-Since' => $last->format('D, d M Y H:i:s') . ' GMT',
+            'HTTP_If-None-Match' => $etag
+        ]);
+        $this->assertEquals(304, $this->client->getResponse()->getStatusCode());
     }
 
 }
