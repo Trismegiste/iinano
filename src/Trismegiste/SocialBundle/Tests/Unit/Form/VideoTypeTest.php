b7f25dbc9444392c2aa191a5b97bba62ff91038a
Trismegiste <flo@trismegiste.net>
2014-11-18 05:20:13 +0100

    preparing video form type tests

---
+++ src/Trismegiste/SocialBundle/Tests/Unit/Form/VideoTypeTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * iinano
+ */
+
+namespace Trismegiste\SocialBundle\Tests\Unit\Form;
+
+use Trismegiste\SocialBundle\Form\VideoType;
+
+/**
+ * VideoTypeTest tests the video form type
+ */
+class VideoTypeTest extends \PHPUnit_Framework_TestCase // PublishingTestCase
+{
+
+    public function test()
+    {
+        $this->markTestIncomplete();
+    }
+
+}
