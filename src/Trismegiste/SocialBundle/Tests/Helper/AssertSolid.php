6a744bd87448443411cb35e490ce61d01a3f3326
Trismegiste <flo@trismegiste.net>
2014-11-21 04:45:37 +0100

    useless optim for solid helper

--- src/Trismegiste/SocialBundle/Tests/Helper/AssertSolid.php
+++ src/Trismegiste/SocialBundle/Tests/Helper/AssertSolid.php
@@ -14,12 +14,12 @@ trait AssertSolid
 
     protected function assertMethodCountEquals($inheritFqcn, array $motherFqcn, $delta = 0)
     {
-        $methodCount = 0;
+        $methodCount = $delta;
         foreach ($motherFqcn as $fqcn) {
             $methodCount += count(get_class_methods($fqcn));
         }
 
-        $this->assertEquals(count(get_class_methods($inheritFqcn)), $methodCount + $delta);
+        $this->assertEquals(count(get_class_methods($inheritFqcn)), $methodCount);
     }
 
 }
