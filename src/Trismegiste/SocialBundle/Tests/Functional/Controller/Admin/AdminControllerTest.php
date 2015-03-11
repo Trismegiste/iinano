5e3016be334e0973ff0d640c0fe3a7d262c6213b
Trismegiste <flo@trismegiste.net>
2015-03-11 13:46:34 +0100

    testing admin dashboard

--- src/Trismegiste/SocialBundle/Tests/Functional/Controller/Admin/AdminControllerTest.php
+++ src/Trismegiste/SocialBundle/Tests/Functional/Controller/Admin/AdminControllerTest.php
@@ -14,10 +14,49 @@ use Trismegiste\SocialBundle\Tests\Functional\Controller\WebTestCasePlus;
 class AdminControllerTest extends WebTestCasePlus
 {
 
-    public function testDashboardSecure()
+    protected $collection;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->collection = $this->getService('dokudoki.collection');
+    }
+
+    /**
+     * @test
+     */
+    public function initialize()
+    {
+        $this->collection->drop();
+        $this->assertCount(0, $this->collection->find());
+        $this->addUserFixture('kirk');
+        $this->addUserFixture('spock');
+
+        $repo = $this->getService('social.netizen.repository');
+        $user = $repo->findByNickname('kirk');
+        $user->setGroup('ROLE_ADMIN');
+        $repo->persist($user);
+    }
+
+    public function testRedirectAccessWithGuest()
     {
         $this->getPage('admin_dashboard');
-        $this->assertNotEquals(200, $this->client->getResponse()->getStatusCode());
+        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testDenyAccessWithSimpleUser()
+    {
+        $this->logIn('spock');
+        $this->getPage('admin_dashboard');
+        $this->assertEquals(403, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testDashboardAccessWithAdmin()
+    {
+        $this->logIn('kirk');
+        $crawler = $this->getPage('admin_dashboard');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $this->assertCount(1, $crawler->filter('div.dashboard-tile:contains("Users")'));
     }
 
 }
