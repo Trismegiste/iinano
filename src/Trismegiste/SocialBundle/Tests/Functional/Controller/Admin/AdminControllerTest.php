0ba9af661366646614a35a7c2be8e85ea8a626b1
Trismegiste <flo@trismegiste.net>
2015-03-14 14:15:35 +0100

    refactoring admin test with new superclass

--- src/Trismegiste/SocialBundle/Tests/Functional/Controller/Admin/AdminControllerTest.php
+++ src/Trismegiste/SocialBundle/Tests/Functional/Controller/Admin/AdminControllerTest.php
@@ -6,55 +6,16 @@
 
 namespace Trismegiste\SocialBundle\Tests\Functional\Controller\Admin;
 
-use Trismegiste\SocialBundle\Tests\Functional\Controller\WebTestCasePlus;
-
 /**
  * AdminControllerTest tests the admin dashboard
  */
-class AdminControllerTest extends WebTestCasePlus
+class AdminControllerTest extends AdminControllerTestCase
 {
-
-    protected $collection;
-
-    protected function setUp()
-    {
-        parent::setUp();
-        $this->collection = $this->getService('dokudoki.collection');
-    }
-
-    /**
-     * @test
-     */
-    public function initialize()
-    {
-        $this->collection->drop();
-        $this->assertCount(0, $this->collection->find());
-        $this->addUserFixture('kirk');
-        $this->addUserFixture('spock');
-
-        $repo = $this->getService('social.netizen.repository');
-        $user = $repo->findByNickname('kirk');
-        $user->setGroup('ROLE_ADMIN');
-        $repo->persist($user);
-    }
-
-    public function testRedirectAccessWithGuest()
-    {
-        $this->getPage('admin_dashboard');
-        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
-    }
-
-    public function testDenyAccessWithSimpleUser()
-    {
-        $this->logIn('spock');
-        $this->getPage('admin_dashboard');
-        $this->assertEquals(403, $this->client->getResponse()->getStatusCode());
-    }
 
     public function testDashboardAccessWithAdmin()
     {
-        $this->logIn('kirk');
-        $crawler = $this->getPage('admin_dashboard');
+        $this->assertSecuredPage('admin_dashboard');
+        $crawler = $this->client->getCrawler();
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
         $this->assertCount(1, $crawler->filter('div.dashboard-tile:contains("Users")'));
     }
