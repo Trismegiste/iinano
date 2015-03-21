9f5834d5aa5aa607f79cf96d4cbad62993785ef0
Trismegiste <flo@trismegiste.net>
2015-03-21 13:36:22 +0100

    fixing test

--- src/Trismegiste/SocialBundle/Cli/MapReduceJob.php
+++ src/Trismegiste/SocialBundle/Cli/MapReduceJob.php
@@ -11,7 +11,6 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Trismegiste\SocialBundle\Repository\MapReduce\RepeatCounter;
 
 /**
  * MapReduceJob is a ...
@@ -25,7 +24,8 @@ class MapReduceJob extends ContainerAwareCommand
     {
         $this->setName('social:mr')
                 ->setDescription('Run a map reduce service')
-                ->addArgument('classname', InputArgument::REQUIRED, "the classname in {$this->namespaceForMruService} namespace");
+                ->addArgument('classname', InputArgument::REQUIRED, "the classname in {$this->namespaceForMruService} namespace")
+                ->addOption('tmpcoll', 'c', InputOption::VALUE_REQUIRED, 'the collection name for the temporary report');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -37,11 +37,16 @@ class MapReduceJob extends ContainerAwareCommand
         if (!is_subclass_of($fqcn, $this->namespaceForMruService . 'MruService')) {
             throw new \InvalidArgumentException("$fqcn is not a valid class");
         }
+        // collection name
+        $tmpCollName = !is_null($input->getOption('tmpcoll')) ? $input->getOption('tmpcoll') : strtolower($input->getArgument('classname'));
+
+        $output->writeln("Lauching $fqcn and creating $tmpCollName");
 
         $refl = new \ReflectionClass($fqcn);
         /** @var $job \Trismegiste\SocialBundle\Repository\MapReduce\MruService */
-        $job = $refl->newInstance($coll, $fqcn);
+        $job = $refl->newInstance($coll, $tmpCollName);
         $job->execute();
+        $output->writeln("Update finished");
     }
 
 }
--- src/Trismegiste/SocialBundle/Tests/Functional/Cli/MapReduceJobTest.php
+++ src/Trismegiste/SocialBundle/Tests/Functional/Cli/MapReduceJobTest.php
@@ -17,36 +17,17 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class MapReduceJobTest extends WebTestCase
 {
 
-    protected $kernel;
-
-    protected function setUp()
-    {
-        parent::setUp();
-        $this->kernel=  $this->createKernel();
-        $kernel->boot();
-    }
-    protected function getService($id){
-        return $this->kernel->getContainer()->get($id);
-    }
-
-    /**
-     * @test
-     */
-    public function initDb()
-    {
-        $coll = $this->getService('dokudoki.collection');
-        $coll->drop();
-        
-    }
-
     public function testExecute()
     {
+        $kernel = static::createKernel();
+        $kernel->boot();
+
         $application = new Application($kernel);
         $application->add(new MapReduceJob());
         $command = $application->find('social:mr');
         $commandTester = new CommandTester($command);
 
-        $commandTester->execute(['command' => $command->getName()]);
+        $commandTester->execute(['command' => $command->getName(), 'classname' => 'RepeatCounter']);
         $this->assertRegExp('#index#', $commandTester->getDisplay());
     }
 
