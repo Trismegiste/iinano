efc2e2d52edbfa9d455e4bf8e28d1b22f8741465
Trismegiste <flo@trismegiste.net>
2015-03-21 13:15:04 +0100

    refactor command for different mru job

--- src/Trismegiste/SocialBundle/Cli/MapReduceJob.php
+++ src/Trismegiste/SocialBundle/Cli/MapReduceJob.php
@@ -19,19 +19,29 @@ use Trismegiste\SocialBundle\Repository\MapReduce\RepeatCounter;
 class MapReduceJob extends ContainerAwareCommand
 {
 
+    protected $namespaceForMruService = 'Trismegiste\SocialBundle\Repository\MapReduce\\';
+
     public function configure()
     {
         $this->setName('social:mr')
-                ->setDescription('Run a map reduce service');
+                ->setDescription('Run a map reduce service')
+                ->addArgument('classname', InputArgument::REQUIRED, "the classname in {$this->namespaceForMruService} namespace");
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         /** @var $coll MongoCollection */
         $coll = $this->getContainer()->get('dokudoki.collection');
-        $job = new RepeatCounter($coll, 'repeat_counter');
-        $job->compileReport();
-        $job->updateRepeat();
+        $fqcn = $this->namespaceForMruService . $input->getArgument('classname');
+        // check
+        if (!is_subclass_of($fqcn, $this->namespaceForMruService . 'MruService')) {
+            throw new \InvalidArgumentException("$fqcn is not a valid class");
+        }
+
+        $refl = new \ReflectionClass($fqcn);
+        /** @var $job \Trismegiste\SocialBundle\Repository\MapReduce\MruService */
+        $job = $refl->newInstance($coll, $fqcn);
+        $job->execute();
     }
 
 }
