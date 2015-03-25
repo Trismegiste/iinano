8aae0111bdfc6a0a91487ab63c2dad0019e73f0b
Trismegiste <flo@trismegiste.net>
2015-03-25 16:00:02 +0100

    adding coupon in the persistance loop

--- src/Trismegiste/SocialBundle/Form/CouponType.php
+++ src/Trismegiste/SocialBundle/Form/CouponType.php
@@ -7,6 +7,7 @@
 namespace Trismegiste\SocialBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 /**
  * CouponType is a form for Coupon entity
@@ -18,13 +19,18 @@ class CouponType extends AbstractType
     {
         $builder->add('duration', 'integer')
                 ->add('maximumUse', 'integer')
-                ->add('expiredAt','date')
+                ->add('expiredAt', 'date')
                 ->add('Create', 'submit');
     }
 
     public function getName()
     {
         return 'free_coupon';
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(['data_class' => 'Trismegiste\SocialBundle\Ticket\Coupon']);
     }
 
 }
