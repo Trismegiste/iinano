584ac124135370d78ec6f44a77c8883093a02c9b
Trismegiste <flo@trismegiste.net>
2015-04-08 22:01:53 +0200

    test for ticket voter

---
+++ src/Trismegiste/SocialBundle/Tests/Unit/Security/TicketVoterTest.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+ * Iinano
+ */
+
+namespace Trismegiste\SocialBundle\Tests\Unit\Security;
+
+use Trismegiste\SocialBundle\Security\TicketVoter;
+use Trismegiste\Socialist\Author;
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+
+/**
+ * OwnerVoterTest tests the voter OwnerVoter
+ */
+class OwnerVoterTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected $sut;
+    protected $currentUser;
+    protected $token;
+
+    protected function createDocument($nick)
+    {
+        return $this->getMockForAbstractClass('Trismegiste\Socialist\Content', [new Author($nick)]);
+    }
+
+    protected function setUp()
+    {
+        $this->sut = new TicketVoter();
+
+        $this->currentUser = $this->getMockBuilder('Trismegiste\SocialBundle\Security\Netizen')
+                ->disableOriginalConstructor()
+                ->getMock();
+        $this->currentUser->expects($this->any())
+                ->method('getAuthor')
+                ->will($this->returnValue(new Author('kirk')));
+
+        $this->token = $this->getMock('Symfony\Component\Security\Core\Authentication\Token\TokenInterface');
+        $this->token->expects($this->any())
+                ->method('getUser')
+                ->will($this->returnValue($this->currentUser));
+    }
+
+    public function testValidTicketGranted()
+    {
+        $this->currentUser->expects($this->any())
+                ->method('hasValidTicket')
+                ->willReturn(true);
+
+        $this->assertEquals(VoterInterface::ACCESS_GRANTED, $this->sut->vote($this->token, null, ['VALID_TICKET']));
+    }
+
+    public function testNoValidTicketDenied()
+    {
+        $this->currentUser->expects($this->any())
+                ->method('hasValidTicket')
+                ->willReturn(false);
+
+        $this->assertEquals(VoterInterface::ACCESS_DENIED, $this->sut->vote($this->token, null, ['VALID_TICKET']));
+    }
+
+    public function testDeniedUnauthenticated()
+    {
+        $anonymous = $this->getMock('Symfony\Component\Security\Core\Authentication\Token\TokenInterface');
+        $this->assertEquals(VoterInterface::ACCESS_DENIED, $this->sut->vote($anonymous, null, ['VALID_TICKET']));
+    }
+
+    public function testAbstainOnUnsupportedAttribute()
+    {
+        $this->assertEquals(VoterInterface::ACCESS_ABSTAIN, $this->sut->vote($this->token, null, ['WESH']));
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testOneAtribute()
+    {
+        $this->assertEquals(VoterInterface::ACCESS_ABSTAIN, $this->sut->vote($this->token, null, ['FLIP', 'FLOP']));
+    }
+
+}
