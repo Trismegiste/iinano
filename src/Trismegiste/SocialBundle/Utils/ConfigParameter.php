b1b4ac0e41aa0ddb24a2bb6360eea5b75d8f23ac
Trismegiste <flo@trismegiste.net>
2015-04-10 12:31:48 +0200

    config parameters class

---
+++ src/Trismegiste/SocialBundle/Utils/ConfigParameter.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * iinano
+ */
+
+namespace Trismegiste\SocialBundle\Utils;
+
+use Trismegiste\Yuurei\Persistence\Persistable;
+
+/**
+ * ConfigParameter is a collection of config parameters (cached)
+ */
+class ConfigParameter implements Persistable
+{
+
+    use \Trismegiste\Yuurei\Persistence\PersistableImpl;
+
+    public $freeAccess = false; // wether this app is free or not
+    public $maintenanceMsg = ''; // global header message
+    public $appTitle = 'iinano'; // title on the front login
+    public $minimumAge = 18;      // minimum age for registering
+
+}
