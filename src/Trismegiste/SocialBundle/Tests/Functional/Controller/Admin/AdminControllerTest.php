d77c2dbbd0a06020d71ce4881c47abcead332c64
Trismegiste <flo@trismegiste.net>
2015-04-21 12:29:21 +0200

    func test admin edit param

--- src/Trismegiste/SocialBundle/Tests/Functional/Controller/Admin/AdminControllerTest.php
+++ src/Trismegiste/SocialBundle/Tests/Functional/Controller/Admin/AdminControllerTest.php
@@ -27,4 +27,29 @@ class AdminControllerTest extends AdminControllerTestCase
         $this->assertCount(1, $crawler->filter('div.dashboard-tile:contains("Users")'));
     }
 
+    public function testSecurityDynamicConfig()
+    {
+        $this->assertSecuredPage('admin_dynamic_config_edit');
+        $crawler = $this->client->getCrawler();
+        $this->assertCount(1, $crawler->filter('div.content form:contains("Free access")'));
+    }
+
+    public function testEditDynamicConfig()
+    {
+        $this->client->followRedirects();
+        $this->logIn('admin');
+
+        $crawler = $this->getPage('admin_dynamic_config_edit');
+        $form = $crawler->filter('div.content form')->form();
+        $randString = 'AZERTY' . rand(); // to find some bug with cache
+        $crawler = $this->client->submit($form, ['dynamic_config' => [
+                'appTitle' => $randString,
+                'minimumAge' => 13,
+                'freeAccess' => 0
+        ]]);
+        sleep(3);
+        $cfg = $this->getService('social.dynamic_config')->read();
+        $this->assertEquals($randString, $cfg['appTitle']);
+    }
+
 }
