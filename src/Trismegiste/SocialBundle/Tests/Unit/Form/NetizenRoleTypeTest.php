6e8a13888a84641626ecd2ed24d94d8d738e261e
Trismegiste <flo@trismegiste.net>
2015-05-09 06:55:46 +0200

    unit test ok for NetizenRoleType

---
+++ src/Trismegiste/SocialBundle/Tests/Unit/Form/NetizenRoleTypeTest.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * Iinano
+ */
+
+namespace Trismegiste\SocialBundle\Tests\Unit\Form;
+
+use Trismegiste\SocialBundle\Form\HierarchyRoleType;
+use Trismegiste\SocialBundle\Form\NetizenRoleType;
+use Trismegiste\SocialBundle\Security\Netizen;
+use Trismegiste\Socialist\Author;
+
+/**
+ * NetizenRoleTypeTest tests NetizenRoleType
+ */
+class NetizenRoleTypeTest extends FormTestCase
+{
+
+    protected $roles = ['ROLE_USER' => [], 'ROLE_ADMIN' => ['ROLE_USER', 'ROLE_PROMOTE']];
+
+    protected function createType()
+    {
+        return [
+            new NetizenRoleType(),
+            new HierarchyRoleType($this->roles)
+        ];
+    }
+
+    protected function createData()
+    {
+        return new Netizen(new Author('kirk'));
+    }
+
+    public function getInvalidInputs()
+    {
+        return [
+            [['group' => 'ab'], new Netizen(new Author('kirk'))]
+        ];
+    }
+
+    public function getValidInputs()
+    {
+        $promoted = new Netizen(new Author('kirk'));
+        $promoted->setGroup('ROLE_ADMIN');
+        return [
+            [['group' => 'ROLE_ADMIN'], $promoted]
+        ];
+    }
+
+}
