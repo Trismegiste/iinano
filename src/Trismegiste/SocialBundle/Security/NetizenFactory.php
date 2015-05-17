984d02a30b54ebb4e84ad2cb135c4c6ea0e49a70
Trismegiste <flo@trismegiste.net>
2015-05-17 04:57:24 +0200

    split the creation of credential from the creation of Netizen  in the NetizenFactory

--- src/Trismegiste/SocialBundle/Security/NetizenFactory.php
+++ src/Trismegiste/SocialBundle/Security/NetizenFactory.php
@@ -18,6 +18,7 @@ use Trismegiste\SocialBundle\Security\Profile;
 class NetizenFactory
 {
 
+    /** @var EncoderFactoryInterface */
     protected $encoderFactory;
 
     public function __construct(EncoderFactoryInterface $encoderFactory)
@@ -38,15 +39,26 @@ class NetizenFactory
         $author = new Author($nick);
         $user = new Netizen($author);
 
-        $salt = \rand(100, 999);
-        $encoded = $this->encoderFactory
-                ->getEncoder($user) // @todo Demeter's law violation : inject encoder as a service with a factory ?
-                ->encodePassword($password, $salt);
-        $user->setCredential(new Internal($encoded, $salt));
+        $this->setNewCredential($user, $password);
         $user->setProfile(new Profile());
         $user->setGroup('ROLE_USER');
 
         return $user;
+    }
+
+    /**
+     * Set a new password to the given user (no persistence of whatsoever)
+     *
+     * @param Netizen $user
+     * @param string $plainPassword
+     */
+    public function setNewCredential(Netizen $user, $plainPassword)
+    {
+        $salt = \rand(100, 999);
+        $encoded = $this->encoderFactory
+                ->getEncoder($user) // @todo Demeter's law violation : inject encoder as a service with a factory ?
+                ->encodePassword($plainPassword, $salt);
+        $user->setCredential(new Internal($encoded, $salt));
     }
 
 }
