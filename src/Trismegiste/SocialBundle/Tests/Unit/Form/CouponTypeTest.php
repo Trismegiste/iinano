4010e5091a03f85527415610563309e8338183d2
Trismegiste <flo@trismegiste.net>
2015-05-19 20:52:40 +0200

    fix unit test of CouponType

--- src/Trismegiste/SocialBundle/Tests/Unit/Form/CouponTypeTest.php
+++ src/Trismegiste/SocialBundle/Tests/Unit/Form/CouponTypeTest.php
@@ -20,15 +20,22 @@ class CouponTypeTest extends FormTestCase
         return new CouponType();
     }
 
+    public function createData()
+    {
+        $obj = new Coupon();
+        $obj->expiredAt = new \DateTime('2015-01-01');
+    }
+
     public function getInvalidInputs()
     {
         $obj = new Coupon();
         $obj->hashKey = 'a';
         $obj->maximumUse = 0;
         $obj->setDurationValue(-5);
+        $obj->expiredAt = new \DateTime('2015-01-01');
         return [
             [
-                ['hashKey' => 'a', 'maximumUse' => 0, 'durationValue' => -5],
+                ['hashKey' => 'a', 'maximumUse' => 0, 'durationValue' => -5, 'expiredAt' => ['year' => 2015, 'month' => 1, 'day' => 1]],
                 $obj,
                 ['hashKey', 'maximumUse', 'durationValue']
             ]
