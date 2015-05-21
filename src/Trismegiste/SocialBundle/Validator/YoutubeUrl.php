55374d6771bc4bd7cb4eab80f1528a8ee15fe6fa
Trismegiste <flo@trismegiste.net>
2015-05-21 05:42:19 +0200

    allow multiple provider for embedded video

--- src/Trismegiste/SocialBundle/Validator/YoutubeUrl.php
+++ src/Trismegiste/SocialBundle/Validator/YoutubeUrl.php
@@ -10,15 +10,10 @@ use Symfony\Component\Validator\Constraints\Url;
 
 /**
  * YoutubeUrl is a validator of a Youtube page. The format is either
- * - http(s)://www.youtube.com/watch?v=xxxxxxxxx
+ * - http(s)://(www.)youtube.com/watch?v=xxxxxxxxx
  * - http(s)://youtu.be/xxxxxxxxx
  */
 class YoutubeUrl extends Url
 {
-
-    public function __construct()
-    {
-
-    }
 
 }
