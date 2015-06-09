6ee685744596574d1ffcf2f219588731c8a3d784
Trismegiste <flo@trismegiste.net>
2015-06-09 16:19:51 +0200

    form type for editing ticket

---
+++ src/Trismegiste/SocialBundle/Form/TicketType.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * iinano
+ */
+
+namespace Trismegiste\SocialBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * TicketType is a type for editing netizen's ticket
+ */
+class TicketType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('expiredAt', 'date');
+    }
+
+    public function getName()
+    {
+        return 'admin_ticket';
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults([
+            'class' => 'Trismegiste\SocialBundle\Ticket\Ticket'
+        ]);
+    }
+
+}
