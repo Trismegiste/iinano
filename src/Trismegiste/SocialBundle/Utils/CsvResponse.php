488793983ea7c1d799bb53a021b1db6ee8b71737
Trismegiste <flo@trismegiste.net>
2015-06-19 11:12:32 +0200

    csv response for big file

---
+++ src/Trismegiste/SocialBundle/Utils/CsvResponse.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+ * iinano
+ */
+
+namespace Trismegiste\SocialBundle\Utils;
+
+use Iterator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+use Symfony\Component\PropertyAccess\PropertyPath;
+
+/**
+ * CsvResponse is a http response in CSV format
+ */
+class CsvResponse extends Response
+{
+
+    /** @var Iterator */
+    protected $iterator;
+
+    /** @var PropertyPath[] */
+    protected $fieldPath = [];
+
+    /** @var PropertyAccessor */
+    protected $propertyAccessor;
+
+    public function __construct(Iterator $content, array $path)
+    {
+        parent::__construct('');
+        $this->iterator = $content;
+        $this->headers->set('Mime-type', 'application/csv');
+        $this->headers->makeDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, 'essai.csv');
+        foreach ($path as $label => $column) {
+            $this->fieldPath[$label] = new PropertyPath($column);
+        }
+        $this->propertyAccessor = new PropertyAccessor();
+    }
+
+    public function setContent($content)
+    {
+        $this->iterator = $content;
+    }
+
+    public function getContent()
+    {
+        ob_start();
+        $this->sendContent();
+
+        return ob_get_clean();
+    }
+
+    public function sendContent()
+    {
+        // header
+        $sepCol = '';
+        echo implode(',', array_keys($this->fieldPath));
+        // rows
+        foreach ($this->iterator as $row) {
+            echo PHP_EOL;
+            $sepCol = '';
+            foreach ($this->fieldPath as $field) {
+                echo $sepCol, $this->getPrintable($this->propertyAccessor->getValue($row, $field));
+                $sepCol = ',';
+            }
+        }
+    }
+
+    private function getPrintable($val)
+    {
+        if (is_string($val)) {
+            $val = '"' . str_replace('"', '\"', $val) . '"';
+        } else if ($val instanceof \DateTime) {
+            $val = '"' . $val->format('Y-m-d H:i:s') . '"';
+        }
+
+        return $val;
+    }
+
+}
