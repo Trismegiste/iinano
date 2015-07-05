102f0d3cf3c7ff18be477273489c64a47965e104
Trismegiste <flo@trismegiste.net>
2015-07-06 01:19:49 +0200

    adding information when func test fails/errors

--- src/Trismegiste/SocialBundle/Tests/Functional/Controller/DumperListener.php
+++ src/Trismegiste/SocialBundle/Tests/Functional/Controller/DumperListener.php
@@ -10,20 +10,22 @@ class DumperListener extends \PHPUnit_Framework_BaseTestListener
     public function addError(\PHPUnit_Framework_Test $test, \Exception $e, $time)
     {
         if (($test instanceof WebTestCasePlus) && !is_null($test->getCurrentResponse())) {
-            $this->dumpResponse('error', $test->getName(), $test->getCurrentResponse());
+            $this->dumpResponse('error', get_class($test), $test->getName(), $test->getCurrentResponse());
         }
     }
 
     public function addFailure(\PHPUnit_Framework_Test $test, \PHPUnit_Framework_AssertionFailedError $e, $time)
     {
         if (($test instanceof WebTestCasePlus) && !is_null($test->getCurrentResponse())) {
-            $this->dumpResponse('failure', $test->getName(), $test->getCurrentResponse());
+            $this->dumpResponse('failure', get_class($test), $test->getName(), $test->getCurrentResponse());
         }
     }
 
-    protected function dumpResponse($prefix, $test, Response $response)
+    protected function dumpResponse($prefix, $fqcn, $test, Response $response)
     {
-        $filepath = sys_get_temp_dir() . sprintf("/%s-%s-%d.html", $prefix, $test, $response->getStatusCode());
+        preg_match('#([^\\\\]+)$#', $fqcn, $match);
+        $shortname = $match[1];
+        $filepath = sys_get_temp_dir() . sprintf("/%s-%s-%s-%d.html", $prefix, $shortname, $test, $response->getStatusCode());
         file_put_contents($filepath, $response->getContent());
     }
 
