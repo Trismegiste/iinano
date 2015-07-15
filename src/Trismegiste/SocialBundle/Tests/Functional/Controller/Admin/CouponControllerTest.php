27e0a44bfc994898081d23668db2dceaba6390fe
Trismegiste <flo@trismegiste.net>
2015-07-15 03:08:57 +0200

    func test for coupon crud action

--- src/Trismegiste/SocialBundle/Tests/Functional/Controller/Admin/CouponControllerTest.php
+++ src/Trismegiste/SocialBundle/Tests/Functional/Controller/Admin/CouponControllerTest.php
@@ -44,4 +44,33 @@ class CouponControllerTest extends AdminControllerTestCase
         $this->assertCount(1, $crawler->filter('div.content td:contains("YUIOP")'));
     }
 
+    public function testEditCoupon()
+    {
+        $this->client->followRedirects();
+        $this->logIn('admin');
+        $crawler = $this->getPage('admin_coupon_listing');
+        $editLink = $crawler->filter('table tr td a[title="Edit"]')->link();
+        $crawler = $this->client->click($editLink);
+        $form = $crawler->filter('div.content form')->form();
+        $crawler = $this->client->submit($form, ['free_coupon' => [
+                'hashKey' => 'EDITED',
+        ]]);
+
+        $this->assertCount(1, $crawler->filter('div.content td:contains("EDITED")'));
+    }
+
+    public function testDeleteCoupon()
+    {
+        $this->client->followRedirects();
+        $this->logIn('admin');
+        $crawler = $this->getPage('admin_coupon_listing');
+        $this->assertCount(3, $crawler->filter('table tr'));
+        $editLink = $crawler->filter('table tr td a[title="Delete"]')->link();
+        $crawler = $this->client->click($editLink);
+        $form = $crawler->filter('div.content form')->form();
+        $crawler = $this->client->submit($form, []);
+
+        $this->assertCount(2, $crawler->filter('table tr'));
+    }
+
 }
