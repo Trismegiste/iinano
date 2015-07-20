09267da0fe5e42d11d783a566d6cac1a1eaada5b
Trismegiste <flo@trismegiste.net>
2015-07-20 08:53:36 +0200

    fix test with new method for social interaction

--- src/Trismegiste/SocialBundle/Tests/Functional/Controller/SocialControllerTest.php
+++ src/Trismegiste/SocialBundle/Tests/Functional/Controller/SocialControllerTest.php
@@ -36,25 +36,25 @@ class SocialControllerTest extends WebTestCasePlus
     public function testLikeSomebody()
     {
         $crawler = $this->getPage('wall_index', ['wallNick' => 'spock', 'wallFilter' => 'self']);
-        $this->assertEquals(0, (int) $crawler->filter('.netizen a.fan-count')->text());
-        $link = $crawler->filter('.netizen a.fan-count')->link();
+        $this->assertEquals(0, (int) $crawler->filter('.netizen button.fan-count')->text());
+        $link = $crawler->filter('.netizen button.fan-count')->form();
         $crawler = $this->client->click($link);
-        $this->assertEquals(1, (int) $crawler->filter('.netizen a.fan-count')->text());
+        $this->assertEquals(1, (int) $crawler->filter('.netizen button.fan-count')->text());
     }
 
     public function testUnlikeSomebody()
     {
         $crawler = $this->getPage('wall_index', ['wallNick' => 'spock', 'wallFilter' => 'self']);
-        $this->assertEquals(1, (int) $crawler->filter('.netizen a.fan-count')->text());
-        $link = $crawler->filter('.netizen a.fan-count')->link();
+        $this->assertEquals(1, (int) $crawler->filter('.netizen button.fan-count')->text());
+        $link = $crawler->filter('.netizen button.fan-count')->form();
         $crawler = $this->client->click($link);
-        $this->assertEquals(0, (int) $crawler->filter('.netizen a.fan-count')->text());
+        $this->assertEquals(0, (int) $crawler->filter('.netizen button.fan-count')->text());
     }
 
     public function testFollowSomebody()
     {
         $crawler = $this->getPage('wall_index', ['wallNick' => 'spock', 'wallFilter' => 'self']);
-        $link = $crawler->filter('.netizen')->selectLink('Follow')->link();
+        $link = $crawler->filter('.netizen')->selectButton('Follow')->form();
         $crawler = $this->client->click($link);
 
         $spock = $this->repo->findByNickname('spock');
@@ -68,7 +68,7 @@ class SocialControllerTest extends WebTestCasePlus
     public function testUnfollowSomebody()
     {
         $crawler = $this->getPage('wall_index', ['wallNick' => 'spock', 'wallFilter' => 'self']);
-        $link = $crawler->filter('.netizen')->selectLink('Unfollow')->link();
+        $link = $crawler->filter('.netizen')->selectButton('Unfollow')->form();
         $crawler = $this->client->click($link);
 
         $spock = $this->repo->findByNickname('spock');
