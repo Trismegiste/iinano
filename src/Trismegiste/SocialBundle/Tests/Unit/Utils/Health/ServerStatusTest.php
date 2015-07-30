dcc4051cb096488e324efeb83a986ac380b911e6
Trismegiste <flo@trismegiste.net>
2015-07-30 05:34:39 +0200

    100% cc for server status

--- src/Trismegiste/SocialBundle/Tests/Unit/Utils/Health/ServerStatusTest.php
+++ src/Trismegiste/SocialBundle/Tests/Unit/Utils/Health/ServerStatusTest.php
@@ -22,10 +22,18 @@ class ServerStatusTest extends \PHPUnit_Framework_TestCase
         $this->sut = new ServerStatus();
     }
 
-    public function testForCC()
+    public function testCpu()
     {
         $this->assertCount(3, $this->sut->getCpuLoad());
+    }
+
+    public function testDisk()
+    {
         $this->assertLessThan(1, $this->sut->getFreeSpaceRatio());
+    }
+
+    public function testMemory()
+    {
         $this->assertArrayHasKey('MemFree', $this->sut->getMemoryLoad());
     }
 
