9a843807043409e9a6ebf1dba9f52f12077d433d
Trismegiste <flo@trismegiste.net>
2015-07-30 05:06:32 +0200

    free space is a better idea

--- src/Trismegiste/SocialBundle/Controller/Admin/AdminController.php
+++ src/Trismegiste/SocialBundle/Controller/Admin/AdminController.php
@@ -39,7 +39,7 @@ class AdminController extends Template
                 'dokudoki' => $dbStatus->getCollectionStats(),
                 'mongo' => $dbStatus->getDbStats(),
                 'memory' => $serverStats->getMemoryLoad(),
-                'usedDisk' => 1 - $serverStats->getFreeSpaceRatio()
+                'freeDisk' => $serverStats->getFreeSpaceRatio()
             ]
         ];
 
