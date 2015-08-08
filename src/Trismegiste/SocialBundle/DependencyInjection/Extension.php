532e61ea5e0fb47975478069b27ac849ce1d8aef
Trismegiste <flo@trismegiste.net>
2015-08-08 10:38:30 +0200

    inject app quota into globals twig

--- src/Trismegiste/SocialBundle/DependencyInjection/Extension.php
+++ src/Trismegiste/SocialBundle/DependencyInjection/Extension.php
@@ -2,18 +2,19 @@
 
 namespace Trismegiste\SocialBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension as BaseExtension;
-use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension as BaseExtension;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class Extension extends BaseExtension
+class Extension extends BaseExtension implements PrependExtensionInterface
 {
 
     /**
@@ -67,6 +68,14 @@ class Extension extends BaseExtension
         if (preg_match('#^Error#', shell_exec('vnstat -i ' . $inet))) {
             throw new InvalidConfigurationException("Network interface '$inet' does not exist");
         }
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        $configs = $container->getExtensionConfig($this->getAlias());
+        $config = $this->processConfiguration(new Configuration(), $configs);
+
+        $container->prependExtensionConfig('twig', ['globals' => ['app_quota' => $config['quota']]]);
     }
 
 }
