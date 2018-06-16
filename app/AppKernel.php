7346b1dffe54f0d405337ca732e7d5bb78cd4928
Trismegiste <flo@trismegiste.net>
2018-06-17 01:12:34 +0200

    removing symfony prelude = not professional

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,17 +1,32 @@
 <?php
 
-use Trismegiste\Prelude\Kernel;
+use Symfony\Component\HttpKernel\Kernel as SymfonyKernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
 
-class AppKernel extends Kernel
-{
+class AppKernel extends SymfonyKernel {
 
-    protected function registerAdditionalBundles()
-    {
-        return [
+    public function registerContainerConfiguration(LoaderInterface $loader) {
+        $loader->load($this->rootDir . '/config/config_' . $this->getEnvironment() . '.yml');
+    }
+
+    public function registerBundles() {
+        $bundles = [
+            new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new \Symfony\Bundle\TwigBundle\TwigBundle(),
+            new \Symfony\Bundle\MonologBundle\MonologBundle(),
+            new \Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Trismegiste\DokudokiBundle\TrismegisteDokudokiBundle(),
             new Trismegiste\OAuthBundle\TrismegisteOAuthBundle(),
             new Trismegiste\SocialBundle\TrismegisteSocialBundle()
         ];
+
+        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+            $bundles[] = new \Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new \Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+        }
+
+        return $bundles;
     }
 
 }
